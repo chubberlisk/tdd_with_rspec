591b0ae06e6f57db47685299152db76b961f83eb
Wen Ting Wang <chubberlisk@outlook.com>
2019-01-07 16:36:40 +0000

    feat: add #full_name_with_middle_initial

--- lib/person.rb
+++ lib/person.rb
@@ -8,4 +8,9 @@ class Person
   def full_name
     [@first_name, @middle_name, @last_name].compact.join(' ')
   end
+
+  def full_name_with_middle_initial
+    return full_name if @middle_name.nil?
+    [@first_name, @middle_name[0].concat('.'), @last_name].compact.join(' ')
+  end
 end
