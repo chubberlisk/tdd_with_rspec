86544c2b7673d23d848d0bd533bd1494615ac915
Wen Ting Wang <chubberlisk@outlook.com>
2019-01-07 16:37:43 +0000

    refactor: remove use of compact in #full_name_with_middle_initial

--- lib/person.rb
+++ lib/person.rb
@@ -11,6 +11,6 @@ class Person
 
   def full_name_with_middle_initial
     return full_name if @middle_name.nil?
-    [@first_name, @middle_name[0].concat('.'), @last_name].compact.join(' ')
+    [@first_name, @middle_name[0].concat('.'), @last_name].join(' ')
   end
 end
